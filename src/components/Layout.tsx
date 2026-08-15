e8996e378caef4b7a3d81cef01ea366fbb1a2135
dk <kudin.dima@gmail.com>
2026-08-15 07:23:33 +0000

    Initial commit

---
+++ src/components/Layout.tsx
@@ -0,0 +1,102 @@
+import FooterSimpleMedia from '@/components/sections/footer/FooterSimpleMedia';
+import NavbarFullscreenStatic from '@/components/ui/NavbarFullscreenStatic';
+import SectionErrorBoundary from "@/components/ui/SectionErrorBoundary";
+import SiteBackgroundSlot from "@/components/ui/SiteBackgroundSlot";
+import { Outlet } from 'react-router-dom';
+import { StyleProvider } from "@/components/ui/StyleProvider";
+
+export default function Layout() {
+  const navItems = [
+  {
+    "name": "Laundry",
+    "href": "#services"
+  },
+  {
+    "name": "Salon",
+    "href": "#services"
+  },
+  {
+    "name": "Games",
+    "href": "#services"
+  },
+  {
+    "name": "Pricing",
+    "href": "#pricing"
+  },
+  {
+    "name": "Hero",
+    "href": "#hero"
+  },
+  {
+    "name": "About",
+    "href": "#about"
+  },
+  {
+    "name": "Metrics",
+    "href": "#metrics"
+  }
+];
+
+  return (
+    <StyleProvider buttonVariant="shift" siteBackground="floatingGradient" heroBackground="gradientBars">
+      <SiteBackgroundSlot />
+      <SectionErrorBoundary name="navbar">
+        <NavbarFullscreenStatic
+      logo="HubServices"
+      ctaButton={{
+        text: "Contact Us",
+        href: "#contact",
+      }}
+     navItems={navItems} />
+      </SectionErrorBoundary>
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <SectionErrorBoundary name="footer">
+        <FooterSimpleMedia
+      brand="HubServices"
+      columns={[
+        {
+          title: "Company",
+          items: [
+            {
+              label: "About",
+              href: "#",
+            },
+            {
+              label: "Team",
+              href: "#",
+            },
+          ],
+        },
+        {
+          title: "Legal",
+          items: [
+            {
+              label: "Privacy",
+              href: "#",
+            },
+            {
+              label: "Terms",
+              href: "#",
+            },
+          ],
+        },
+      ]}
+      copyright="© 2024 HubServices. All rights reserved."
+      links={[
+        {
+          label: "Twitter",
+          href: "#",
+        },
+        {
+          label: "Instagram",
+          href: "#",
+        },
+      ]}
+      imageSrc="http://img.b2bpic.net/free-photo/dining-sitting-furniture-set-cafe-restaurant-with-light-colors-big-windows_114579-1522.jpg"
+    />
+      </SectionErrorBoundary>
+    </StyleProvider>
+  );
+}
